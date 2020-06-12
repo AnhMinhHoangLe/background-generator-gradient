32e64cf290aed229f7f4a6058ae62df72bcee061
AnhMinhHoangLe <leanh6998@gmail.com>
2020-06-12 04:51:23 -0700

    final

---
+++ README.md
@@ -0,0 +1,12 @@
+# background-generator-gradient
+
+## `About`
+
+- The background generator is a small and simple application to make a color gradient. <br>
+- Users are able to hand-pick two colors or let the application automatically pick two random colors.
+
+## `Demo`
+
+One of examples of the application
+
+![Demo Image](./image/Capture.png)
--- scripts.js
+++ scripts.js
@@ -7,25 +7,23 @@ var button = document.getElementById("button");
 // console.log(color1)
 // console.log(color2)
 // console.log(bodyy)
-// color1.addEventListener("input", changeColor); 
-// color2.addEventListener("input",changeColor); 
-button.addEventListener('click', random);
+// color1.addEventListener("input", changeColor);
+// color2.addEventListener("input",changeColor);
+button.addEventListener("click", random);
 function changeColor() {
-        body.style.background = "linear-gradient(to right, "
-                + color1.value
-                + ", "
-                + color2.value
-                + ")";
-        css.textContent = body.style.background
+  body.style.background =
+    "linear-gradient(to right, " + color1.value + ", " + color2.value + ")";
+  //   css.textContent = body.style.background;
+    css.textContent = ` Hex color: ${color1.value} and  ${color2.value}`;
 }
 function randomString() {
-        var randomColor;
-        randomColor = Math.floor(Math.random() * 999999);
-        return randomColor;
+  var randomColor;
+  randomColor = Math.floor(Math.random() * 999999);
+  return randomColor;
 }
 function random() {
-        color1.value = '#' + randomString()
-        color2.value = '#' + randomString()
-        changeColor()
+  color1.value = "#" + randomString();
+  color2.value = "#" + randomString();
+  changeColor();
 }
-changeColor()
+changeColor();
